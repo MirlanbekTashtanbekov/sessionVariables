96023634daa0d4668b194866a673a2f875185116
mtash <mtashtanbekov@gmail.com>
2024-10-02 13:42:46 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,134 @@
+public class Main {
+    public static void main(String[] args) {
+
+//  TODO Task 1
+        /*int a = 5;
+        int b1 = 1;
+        int b2 = 2;
+        int b3 = 3;
+        int b4 = 4;
+        int b5 = 5;
+        int b6 = 6;
+        int b7 = 7;
+        int b8 = 8;
+        int b9 = 9;
+
+        System.out.println("5 тин таблица умножениясы:");
+        System.out.println(a+"*"+b1+"="+(a*b1));
+        System.out.println(a+"*"+b2+"="+(a*b2));
+        System.out.println(a+"*"+b3+"="+(a*b3));
+        System.out.println(a+"*"+b4+"="+(a*b4));
+        System.out.println(a+"*"+b5+"="+(a*b5));
+        System.out.println(a+"*"+b6+"="+(a*b6));
+        System.out.println(a+"*"+b7+"="+(a*b7));
+        System.out.println(a+"*"+b8+"="+(a*b8));
+        System.out.println(a+"*"+b9+"="+(a*b9));*/
+
+// TODO Task 2
+
+/*      System.out.println("""
+              **********   *               *
+              **********   **             **
+              **********   ***           ***
+              **********   ****         ****
+                           *****       *****
+              
+                  *             1          1
+                 ***           222        212
+                *****         33333      32123
+               *******       4444444    4321234
+              *********     555555555  543212345
+              """);*/
+
+// TODO Task 3 Homework
+        /*String day1 = "Monday";
+        String day2 = "Tuesday";
+        String day3 = "Wednesday";
+        String date09 = "2024-01-09";
+        String date10 = "2024-01-10";
+        String date11 = "2024-01-11";
+        String lessonEn = "Technical English";
+        String lessonPr = "Practical lesson";
+        String lessonTec = "Technical lesson";
+        char z = '-';
+        char i = ':';
+        int a14 = 14;
+        int a16 = 16;
+        int a18 = 18;
+        int a20 = 20;
+        int a30 = 30;
+        int a00 = 0;
+        int a01 = 0;
+
+        System.out.println("*      " + day1 + "             *");
+        System.out.println("*     " + date09 + "          *");
+        System.out.println("*     " + a14 + i + a30 + z + a16 + i + a00 + a01 + "         *");
+        System.out.println("*     " + lessonEn + "   *");
+        System.out.println("*                         *");
+        System.out.println("*                         *");
+        System.out.println("*                         *");
+        System.out.println("***************************");
+        System.out.println("*      " + day2 + "            *");
+        System.out.println("*     " + date10 + "          *");
+        System.out.println("*     " + a18 + i + a00 + a01 + z + a20 + i + a00 + a01 + "         *");
+        System.out.println("*     " + lessonPr + "    *");
+        System.out.println("*                         *");
+        System.out.println("*                         *");
+        System.out.println("*                         *");
+        System.out.println("***************************");
+        System.out.println("*       " + day3 + "         *");
+        System.out.println("*      " + date11 + "         *");
+        System.out.println("*      " + a18 + i + a00 + a01 + z + a20 + i + a00 + a01 + "        *");
+        System.out.println("*      " + lessonTec + "   *");
+        System.out.println("*                         *");
+        System.out.println("*                         *");
+        System.out.println("*                         *");*/
+
+// TODO Homework Task 1
+        /*int a = 100; // тик бурчтун узундугу
+        int b = 50;  // тик бурчтун туурасы
+        int s = a * b; // тик бурчтун аянтын табууда S=a*b
+
+        System.out.println("Тик бурчтуктун узундугу 100");
+        System.out.println("тик бурчтуктун туурасы 50");
+        System.out.println("тик бурчтуктун аянты = " + s);*/
+
+// TODO Homework Task 2 Draft-1
+        /*int year1 = 2020;
+        int year2 = 2021;
+        int a1 = 70;
+        int a2 = 80;
+        String camp = "кампус";
+        String j = "Java";
+        String js = "JavaScript";
+        char z = '%';
+
+        System.out.printf("""
+                "Peaksoft House %s - жылы ноябрь айында негизделген. Эн алгач окутуу
+                онлайн форматында гана өткөрүлгөн. %S - жылы июнь айында Бишкекте
+                %s ачылып, окутуунун офлайн форматы дагы киргизилген. Бүгүнкү күндө
+                Peaksoft House тан %s жана %s тилдери боюнча студенттер ийгиликтүү
+                окууну аяктап, %s - %s %sти IT адистери болуп эмгектенип жатышат".
+                """, year1, year2, camp, j, js, a1, a2, z);*/
+
+// TODO Homework Task 2 Draft-2
+        /*int year1 = 2020;
+        int year2 = 2021;
+        int a1 = 70;
+        int a2 = 80;
+        String camp = "кампус";
+        String j = "Java";
+        String js = "JavaScript";
+
+        System.out.println("Peaksoft House " + year1 + " - жылы ноябрь айында негизделген. Эн алгач окутуу\n" +
+                "онлайн форматында гана өткөрүлгөн. " + year2 + " - жылы июнь айында Бишкекте\n" +
+                "" + camp + " ачылып, окутуунун офлайн форматы дагы киргизилген. Бүгүнкү күндө\n" +
+                "Peaksoft House тан " + j + " жана " + js + " тилдери боюнча студенттер ийгиликтүү\n" +
+                "окууну аяктап, " + a1 + " - " + a2 + " %ти IT адистери болуп эмгектенип жатышат.");*/
+
+
+
+
+
+    }
+}
